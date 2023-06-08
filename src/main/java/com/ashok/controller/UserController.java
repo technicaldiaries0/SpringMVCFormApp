6eecdda58959ced20257960fc6c33282a145a027
technicaldiaries0 <133200869+technicaldiaries0@users.noreply.github.com>
2023-06-09 01:39:37 +0530

    Update UserController.java

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -13,7 +13,7 @@ public class UserController {
 
 	@RequestMapping(value = "/displayForm.htm", method = RequestMethod.GET)
 	public String displayForm(Model model) {
-		System.out.println("hi");
+		System.out.println("hi this my new jenkin project");
 		int i = 10;
 		model.addAttribute("user", new User());
 		return "userForm";
